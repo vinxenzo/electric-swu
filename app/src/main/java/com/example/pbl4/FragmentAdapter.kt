57600d4859ba13a193e98a2fee4d72963c2a6f2c
vinxenzo <wonhostred93@gmail.com>
2023-09-25 01:55:16 +0900

    0925 디자인 수정

---
+++ app/src/main/java/com/example/pbl4/FragmentAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.pbl4
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class FragmentAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+
+    var fragmentList = listOf<Fragment>()
+
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList.get(position)
+    }
+
+}
--- app/src/main/java/com/example/pbl4/MainPage.kt
+++ app/src/main/java/com/example/pbl4/MainPage.kt
@@ -3,9 +3,18 @@ package com.example.pbl4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainPage : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+
+        val fragmentList = listOf(BuyFragment(), SellFragment(), OutstandingFragment())
+        val adapter = FragmentAdapter(this)
+        adapter.fragmentList = fragmentList
+        binding.viewPager.adapter = adapter
+
     }
 }
